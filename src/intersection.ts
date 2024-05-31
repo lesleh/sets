94ad59a0527e1097aacc8bbe2249bda44d4d02d8
Leslie Hoare <iam@lesleh.co.uk>
2024-05-31 17:30:11 +0100

    Add docs for intersection function (#18)

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,3 +1,11 @@
+/**
+ * Computes the intersection of two sets.
+ * @param a The first set.
+ * @param b The second set.
+ * @returns A new set containing the elements that are common to both input sets.
+ * @throws {Error} If either `a` or `b` is not a Set.
+ * @template T The type of elements in the sets.
+ */
 export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
   if (!(a instanceof Set) || !(b instanceof Set)) {
     throw new Error("Both arguments must be sets.");
